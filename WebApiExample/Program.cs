237a59b9a5c072b6f678f4523c2cc5b8fd3761ef
Meinrad Recheis <meinrad.recheis@gmail.com>
2020-02-16 21:57:41 +0100

    Examples: added a very simple .NET Core Web API example

---
+++ WebApiExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Numpy;
+
+namespace WebApiExample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // this call initializes numpy. it is necessary to do that before PythonEngine.BeginAllowThreads()
+            np.arange(1);
+            Python.Runtime.PythonEngine.BeginAllowThreads(); // <--- this is very important for a web server since all requests are on different threads
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
