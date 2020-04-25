a50ec8132c7e8fbb4796925069941471f1e88959
Meinrad Recheis <meinrad.recheis@gmail.com>
2020-04-25 13:00:34 +0200

    Examples: Added a simple NetCore test program

---
+++ src/Examples/NetCoreTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Numpy;
+
+namespace NetCoreTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var m = np.array(new int[] { 1, 2, 3, 4 }) * 2;
+            Console.WriteLine(m.repr);
+
+            Console.WriteLine("");
+            Console.WriteLine("Press any key to exit..");
+            Console.ReadKey();
+        }
+    }
+}
