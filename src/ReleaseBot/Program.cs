19d9e83076850e1f6ac8a527d9fe7a35d1f31e23
Meinrad Recheis <meinrad.recheis@gmail.com>
2022-01-26 13:33:53 +0100

    v1.28

--- src/ReleaseBot/Program.cs
+++ src/ReleaseBot/Program.cs
@@ -13,7 +13,7 @@ namespace ReleaseBot
 {
     class Program
     {
-        private const string V = "1.27"; // <--- numpy.net version!
+        private const string V = "1.28"; // <--- numpy.net version!
         private const string PythonNetVersion = "2.5.1";
 
         private const string ProjectPath = "../../../Numpy";
