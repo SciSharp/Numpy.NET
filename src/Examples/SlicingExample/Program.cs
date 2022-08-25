8a0115a341de5ba289b207c39be8481a16121096
Meinrad Recheis <meinrad.recheis@gmail.com>
2022-08-25 09:30:16 +0200

    Added new Slicing example (.net5)

---
+++ src/Examples/SlicingExample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Numpy;
+
+namespace SlicingExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = np.arange(20).reshape(4,5);
+            Console.WriteLine(a);
+            var b = a["2:4"];
+            Console.WriteLine("\n sliced with 2:4");
+            Console.WriteLine(b );
+        }
+    }
+}
